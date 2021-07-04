582fb99ff0d4fc2f4f0d450e4f6bc46dfd520562
Eric Loos <eric@ipergy.net>
2021-07-04 18:23:38 +0200

    update 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Dangerous World!'));
+app.get('/', (req, res) => res.send('Hello Confusingly Dangerous World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
